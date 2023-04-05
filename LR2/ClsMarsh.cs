5dd3600970a243e4f53f97c82c099d429fc3d582
sarvinduri <s.ramm.s.ramm@gmail.com>
2023-04-05 09:46:31 +0300

    Добавьте файлы проекта.

---
+++ LR2/ClsMarsh.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR2
+{
+    internal class ClsMarsh
+    {
+        private string startA;
+        private string endB;
+        private string number;
+        // свойства для доступа к полям
+        public string StartA
+        {
+            get { return startA; }//возвращает значение поля
+            set { startA = value; } //устанавливает значение поля
+        }
+        public string EndB
+        {
+            get { return endB; }
+            set { endB = value; }
+        }
+        public string Number
+        {
+            get { return number; }
+            set { number = value; }
+        }
+
+        //методы 
+        //конструктор без параметров задает значения полей по умолчанию
+        public ClsMarsh()
+        {
+            startA = "";
+            endB = "";
+            endB = "";
+        }
+        //конструктор с параметрами
+        public ClsMarsh(string A, string n, string B)
+        {
+            startA = A;
+            endB = B;
+            endB = n;
+        }
+        //метод вывода на экран
+        public string PrintToScreen()
+        {
+            string line = $"Маршрут начинается: {startA} маршрут заканчивается: {endB}" + "\n";
+            line += $"Номер маршрута: {number}" + "\n";
+
+            return line;
+        }
+        public void SaveToFile(string filename)
+        {
+            StreamWriter streamWriter = new StreamWriter(filename, true, Encoding.Default);
+            streamWriter.WriteLine("Маршрут");
+            streamWriter.WriteLine($"Начало маршрута: {startA}");
+            streamWriter.WriteLine($"Конец маршрута: {endB.ToString()}");
+            streamWriter.WriteLine($"Номер маршрута: {number.ToString()}");
+            streamWriter.Close();
+        }
+    }
+}
---
+++ LR2/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace LR2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSavestudent_Click(object sender, RoutedEventArgs e)
+        {
+            ClsMarsh Marsh = new ClsMarsh();
+            Marsh.StartA = TxtA.Text;
+            Marsh.EndB = TxtB.Text;
+            Marsh.Number = TxtStip.Text;
+
+            LstStudents.Items.Clear();
+            LstStudents.Items.Add(Marsh.PrintToScreen());
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == false)
+                return;
+            Marsh.SaveToFile(saveFileDialog.FileName);
+
+        }
+    }
+}
